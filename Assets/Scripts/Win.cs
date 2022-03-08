9491303ee43fd3b360be09ef2f5a4d2a52e4b1e1
Arjun-Ajmani <89949587+Arjun-Ajmani@users.noreply.github.com>
2022-03-07 16:52:10 -0800

    animation stuf

--- Assets/Scripts/Win.cs
+++ Assets/Scripts/Win.cs
@@ -19,7 +19,7 @@ public class Win : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("House"))
+        if (collision.gameObject.CompareTag("Player"))
         {
             SceneManager.LoadScene("Win");
         }
