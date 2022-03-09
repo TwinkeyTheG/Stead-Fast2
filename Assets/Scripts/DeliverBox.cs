ef01f63ffb9a254b76052fe4ff5c463946eed211
TwinkeyTheG <90072473+TwinkeyTheG@users.noreply.github.com>
2022-03-08 16:24:15 -0800

    made some changes to the data structures for the gamemanager

--- Assets/Scripts/DeliverBox.cs
+++ Assets/Scripts/DeliverBox.cs
@@ -7,7 +7,6 @@ public class DeliverBox : MonoBehaviour
 {
     public Terminal myBox;
     public bool PackageDelivered = false;
-    public int TruckCount = 0;
     // Start is called before the first frame update
     void Start()
     {
